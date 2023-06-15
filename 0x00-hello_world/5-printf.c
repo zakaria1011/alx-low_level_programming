05ef60e8c4e40f7ee056687aeeca42fde4006215
zakaria1011 <grarizakaria@gmail.com>
2023-06-15 13:02:30 -0700

    alx

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - main block
  * Return: 0
@@ -6,5 +6,5 @@
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0); 
+return (0);
 }
