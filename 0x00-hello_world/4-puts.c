40b2e3b2537dc14af75ce63d6560d137c6c9eb7d
zakaria1011 <grarizakaria@gmail.com>
2023-06-15 12:50:05 -0700

    alx

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
-#include<stdio.h>
-int main (void)
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (0); 
+return (0);
 }
