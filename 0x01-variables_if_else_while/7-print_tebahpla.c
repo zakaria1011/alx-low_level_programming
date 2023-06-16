d16c7966a156b47aa97891e5ae5f2c1449bd4f99
zakaria1011 <grarizakaria@gmail.com>
2023-06-16 15:06:10 -0700

    alx

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  * main - main block
- * Description: alphabet invert order
- * alpha bet invert 
+ * Description: prints all single digit numbers of base 10
+ * starting from 0, followed by a new line.
  * Return: 0
  */
 int main(void)
