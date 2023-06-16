27f193aad2e0c2da102a1d30a96b72397abd6fd6
zakaria1011 <grarizakaria@gmail.com>
2023-06-16 14:52:01 -0700

    alx

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,18 +1,20 @@
 #include<stdio.h>
 /**
- * main
- * alphabet invert 
+ * main- capital block
+ * description: alphabet invert 
  * return 0
  */
-int main (void )
+int main(void)
 {
 	char c='z';
+
 	while (c>='a')
 	{
 		putchar(c);
 		c--;
-	
 	}
+
 putchar('\n');
+
 return (0);
 }
