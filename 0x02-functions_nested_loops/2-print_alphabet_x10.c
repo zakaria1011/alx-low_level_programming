108480686257ed4c54de03159733b71b26336638
zakaria1011 <grarizakaria@gmail.com>
2023-06-20 17:53:49 -0700

    alx

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,7 @@ void print_alphabet_x10(void)
 	char ch;
 	int i;
 
-	i = 0;
-
-	while (i < 10)
+for (i = 0;i < 10; i++)
 	{
 		ch = 'a';
 		while (ch <= 'z')
@@ -20,6 +18,6 @@ void print_alphabet_x10(void)
 			ch++;
 		}
 		_putchar('\n');
-		i++;
+
 	}
 }
