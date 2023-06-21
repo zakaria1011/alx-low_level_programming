594c4815ec05c3258c92bb61bb9bf84cd0bb74e3
zakaria1011 <grarizakaria@gmail.com>
2023-06-20 17:20:20 -0700

    alx

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - printf alphabet 10 times.
+ */
+void print_alphabet_x10(void)
+{
+
+int i ;
+
+for(i=0; i<10 ; i++) 
+{
+    char c = 'a';
+
+    while (c <= 'z')
+    {
+        putchar(c);
+        c++;
+    }
+
+    putchar('\n');
+}
