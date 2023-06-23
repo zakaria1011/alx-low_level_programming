611b4e80a84f00ad0da6f3c16b69130328abfad3
zakaria1011 <grarizakaria@gmail.com>
2023-06-22 19:31:24 -0700

    alx

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "main"
+
+/**
+ * print_diagonal - printf diagonal
+ *@n: number od slash
+ * Return : nothing
+ */
+
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+
+	int i;
+
+	int j;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		_putchar('\\')
+			_putchar('\n');
+	for (j = 0 ; j <= i ; j++)
+	{
+		_putchar(' ');
+	}
+	}
+	}
+	_putchar('\n');
+}
