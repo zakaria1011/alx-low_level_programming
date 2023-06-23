e4771351652b63f49c2e89b966fd8dd8a7e096bc
zakaria1011 <grarizakaria@gmail.com>
2023-06-22 17:52:56 -0700

    alx

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - print all num exept 2 and 4
+ * Return: nothing
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		if (i == 4 && i == 8)
+		{
+			continue;
+		}
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
