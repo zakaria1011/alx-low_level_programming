1fc5dd135b11a4e3ed122afabfcd4cdd966e8fa6
zakaria1011 <grarizakaria@gmail.com>
2023-06-22 19:49:31 -0700

    alx

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_triangle - check the code for Holberton School students.
+ *@size: variable
+ * Return: Always 0.
+ */
+void print_triangle(int size)
+{
+int count, i;
+count = 0;
+if (size > 0)
+{
+while (count < size)
+{
+i = size;
+while (i > count + 1)
+{
+i--;
+_putchar(' ');
+}
+while (i > 0)
+{
+_putchar('#');
+i--;
+}
+count++;
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
