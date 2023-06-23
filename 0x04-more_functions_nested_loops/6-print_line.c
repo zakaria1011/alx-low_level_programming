173d44e12b82803adc2db152404251cb9e6bed09
zakaria1011 <grarizakaria@gmail.com>
2023-06-22 18:41:47 -0700

    alx

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - print a line
+ *@n: lenght of line
+ * Return: nothing
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
+
+
