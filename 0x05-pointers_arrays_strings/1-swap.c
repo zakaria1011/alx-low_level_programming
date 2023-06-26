c223aeae7a7eff1058f11ceeb3d1aa7ae8cadaa4
zakaria1011 <grarizakaria@gmail.com>
2023-06-26 16:31:59 -0700

    alx

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * swap_int - swaping tw numbers
+ *@a: first argment
+ *@b: second argument
+ *Return: nothing
+ */
+void swap_int (int *a, int *b)
+{
+	int *p;
+	*p = *a;
+	*a = *b;
+	*b = *p;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,5 +2,5 @@
 #define MAIN_H
 
 void reset_to_98(int *p);
-
+void swap_int(int *a, int *b);
 #endif
