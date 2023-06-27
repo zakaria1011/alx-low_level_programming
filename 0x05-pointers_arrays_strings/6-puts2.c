da638a658b644131b45894d392a8a1798625f452
zakaria1011 <grarizakaria@gmail.com>
2023-06-26 19:41:26 -0700

    alx

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * puts2 - put add caracter
+ *@str: argument
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	int i;
+	int len;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	for (i = 0 ; i < len ; i++)
+	{
+		if (i % 2 != 0)
+		{
+			continue;
+		}
+		else
+		{
+			printf("%c", str[i]);
+		}
+	}
+}
