c974bde40dfaaae5b247fca77fb3119d75e498d0
zakaria1011 <grarizakaria@gmail.com>
2023-06-26 18:06:59 -0700

    alx

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * print_rev - printstring in reverse
+ *@str: argument
+ * Return: nothing
+ */
+void print_rev(char *str)
+{
+	int len;
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	while (len != -1)
+	{
+		printf("%c", str[len]);
+		len--;
+	}
+	printf("\n");
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,4 +5,5 @@ void reset_to_98(int *p);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
+void print_rev(char *str);
 #endif
