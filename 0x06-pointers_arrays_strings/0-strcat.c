fabb06258272ba7cf8e86c0ad0c2ed2731c2bfc2
zakaria1011 <grarizakaria@gmail.com>
2023-06-29 05:40:39 -0700

    alx

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strcat - add two word
+ *@dest: first arg
+ *@src: second arg
+ * Return: returndest
+ */
+
+char *_strcat(char *dest, *src)
+{
+	int len1;
+	int len2;
+
+	len1=0;
+	len2=0;
+	while(dest[len1]!='\0')
+	{
+		len++;
+	}
+	while(src[len2]!='\0')
+	{
+		*(dest + (len1 + len2)) = *(src + len2);
+		len2++;
+	}
+	*(dest + (len1 + len2 + 1))='\0';
+	return (dest);
+}
+	
