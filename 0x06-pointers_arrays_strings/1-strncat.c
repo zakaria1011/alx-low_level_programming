aa314eef82ef1b2aac1d89ea8a6752f50be9ccdb
zakaria1011 <grarizakaria@gmail.com>
2023-06-29 20:19:33 +0100

    alx

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,15 +13,18 @@ char *_strncat(char *dest, char *src, int n)
 
 i = 0;
 j = 0;
+
 while (dest[i] != '\0')
 {
     i++;
     }
+
 while (j < n)
 {
     dest[i] = src[j];
     j++;
     i++;
     }
+    
 return (dest);
 }
