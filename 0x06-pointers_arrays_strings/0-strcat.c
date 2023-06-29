f397647567039d440dc0e3b8d476b13bd296a8cd
zakaria1011 <grarizakaria@gmail.com>
2023-06-29 19:23:38 +0100

    alx

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,24 +5,23 @@
  *@src: second arg
  * Return: returndest
  */
-
 char *_strcat(char *dest,char *src)
 {
 	int len1;
 	int len2;
-
+	
 	len1 = 0;
 	len2 = 0;
 	while (dest[len1] != '\0')
 	{
 		len1++;
-	}
+		}
 	while (src[len2] != '\0')
 	{
 		*(dest + len1) = *(src + len2);
 		len2++;
 		len1++;
-	}
+		}
 	*(dest + len1) = '\0';
 	return (dest);
 }
