e8fe16a7165a5c92793449ae41d57764c70d4852
zakaria1011 <grarizakaria@gmail.com>
2023-06-29 16:27:17 -0700

    alx

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+ /**
+  *_strncpy - copie a string
+  *@dest: first argumen
+  *@src: second argumant
+  *@n: number to copy
+  *Return: dest
+  */
+char *_strncpy(char *dest, char *src, int  n)
+{
+	int j;
+
+	j = 0;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	dest[j] = '\0';
+	return (dest);
+}
