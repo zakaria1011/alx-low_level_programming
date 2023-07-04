926d8bd49577d14d42dfbce33375cbc623656d9c
zakaria1011 <grarizakaria@gmail.com>
2023-07-04 03:25:26 +0100

    alx

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,7 +11,7 @@
 char *_strstr(char *haystack, char *needle)
 {
 	if (*needle == '\0')
-		return haystack;
+		return (haystack);
 
 	while (*haystack)
 	{
@@ -25,7 +25,7 @@ char *_strstr(char *haystack, char *needle)
 		}
 
 		if (*n == '\0')
-			return haystack;
+			return (haystack);
 
 		haystack++;
 	}
