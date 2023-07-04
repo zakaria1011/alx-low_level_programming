657c6b9da0ad6fc8b30db44f2d72e96da3280978
zakaria1011 <grarizakaria@gmail.com>
2023-07-04 01:11:03 +0100

    alx

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,20 +8,15 @@
 char *_strchr(char *s, char c)
 {
 if (s == NULL)
-{
 return (NULL);
-}
+
 while (*s != '\0')
 {
 if (*s == c)
-{
 return (s);
-}
 s++;
 }
 if (c == '\0')
-{
 return (s);
-}
 return (NULL);
 }
