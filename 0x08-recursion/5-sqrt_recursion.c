475b63f7341c6f152af95654094013aad5cf00b0
zakaria1011 <grarizakaria@gmail.com>
2023-07-05 20:55:34 +0100

    alc

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * _sqrt_recursion - calcul squar root
+ * @n: argument
+ * Return: sqrt root
+ */
+int _sqrt_recursion(int n)
+{
+unsigned int i = n;
+if (n < 0)
+{
+return (-1);
+}
+while (i * i != n && i != 0)
+{
+i--;
+}
+if (i == 0)
+{
+return (-1);
+}
+else
+{
+return (i);
+}
+}
