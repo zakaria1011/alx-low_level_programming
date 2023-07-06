a4ddf65c327e0a167ce6a9ccf38d050d97f228f5
zakaria1011 <grarizakaria@gmail.com>
2023-07-06 01:11:07 +0100

    alx

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,28 +1,36 @@
 #include "main.h"
-
 /**
- * _sqrt_recursion - main funct
- * @n: int n
- * Return: int
+ * _sqrt_recursion_helper - Recursive helper
+ * @n: The number for which to find the square root
+ * @start: The starting index for the search range
+ * @end: The ending index for the search range
+ * Return: The natural square root of the number, -1
+ */
+int _sqrt_recursion_helper(int n, int start, int end)
+{
+	if (start > end)
+		return (-1);
+
+	int mid = (start + end) / 2;
+	int square = mid * mid;
+
+	if (square == n)
+		return (mid); /* Natural square root found */
+	else if (square > n)
+		return (_sqrt_recursion_helper(n, start, mid - 1));
+	else
+		return (_sqrt_recursion_helper(n, mid + 1, end));
+}
+/**
+ * _sqrt_recursion - Calculates the natural square root
+ * @n: The number to calculate the square root of
+ *
+ * Return: The natural square root of the number, -1 if no
  */
 int _sqrt_recursion(int n)
 {
-	return (_sqrt(n, 1));
-}
-
-/**
- * _sqrt - _sqrt_recursion
- * @n: integer paramtr
- * @i: integer parameter
- * Return: sqrt
- */
-int _sqrt(int n, int i)
-{
 	if (n < 0)
-		return (-1);
-	if ((i * i) > n)
-		return (-1);
-	if (i * i == n)
-		return (i);
-	return (_sqrt(n, i + 1));
+		return (-1); /* Invalid input, negative number */
+
+	return (_sqrt_recursion_helper(n, 0, n));
 }
