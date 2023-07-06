d83cd61a7e6543a4fe66eb710b7f213f5a668b88
zakaria1011 <grarizakaria@gmail.com>
2023-07-06 19:31:46 +0100

    alx

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,7 +6,7 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _sqrt_recursion(int n);
-int is_prime_number(int n, int i);
+int is_prime_number(int n);
 int is_palindrome(char *s);
 int _sqrt_recursion_helper(int n, int start);
 #endif 
