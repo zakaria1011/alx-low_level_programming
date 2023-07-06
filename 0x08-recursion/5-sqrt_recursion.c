f531b2b746899a98b1f9469dfb949da860b767e5
zakaria1011 <grarizakaria@gmail.com>
2023-07-06 18:32:28 +0100

    alx

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,36 +1,39 @@
 #include "main.h"
+#include<stdio.h>
 /**
- * _sqrt_recursion_helper - Recursive helper
- * @n: The number for which to find the square root
- * @start: The starting index for the search range
- * @end: The ending index for the search range
- * Return: The natural square root of the number, -1
+ * _sqrt_recursion_helper - help in calcul
+ * @n: argument
+ * @start: compteur
+ * Return: return the root
  */
-int _sqrt_recursion_helper(int n, int start, int end)
+int _sqrt_recursion_helper(int n, int start)
 {
-	if (start > end)
-		return (-1);
-
-	int mid = (start + end) / 2;
-	int square = mid * mid;
-
-	if (square == n)
-		return (mid); /* Natural square root found */
-	else if (square > n)
-		return (_sqrt_recursion_helper(n, start, mid - 1));
-	else
-		return (_sqrt_recursion_helper(n, mid + 1, end));
+if (start == n)
+{
+return (-1);
+}
+else if (start * start == n)
+{
+return (start);
+}
+else
+{
+return (_sqrt_recursion_helper(n, start + 1));
+}
 }
 /**
- * _sqrt_recursion - Calculates the natural square root
- * @n: The number to calculate the square root of
- *
- * Return: The natural square root of the number, -1 if no
+ * _sqrt_recursion - main fucntio
+ * @n: argument
+ * Return: return root
  */
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1); /* Invalid input, negative number */
-
-	return (_sqrt_recursion_helper(n, 0, n));
+if (n < 0)
+{
+return (-1);
+}
+else
+{
+return (_sqrt_recursion_helper(n, 0));
+}
 }
