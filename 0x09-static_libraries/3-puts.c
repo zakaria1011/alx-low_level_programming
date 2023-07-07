f54fb847b38e5c08cee512908e862c5b0b95fe0b
zakaria1011 <grarizakaria@gmail.com>
2023-07-07 11:39:49 +0100

    akx

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,19 +1,16 @@
 #include "main.h"
-#include<stdio.h>
+
 /**
- * _puts - print string
- *@str: argument
- *Return: nothing
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
  */
 void _puts(char *str)
 {
-	int len;
+	int i;
 
-	len = 0;
-	while (str[len] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		printf("%c", str[len]);
-		str++;
+		_putchar(str[i]);
 	}
-	printf("\n");
+	_putchar('\n');
 }
