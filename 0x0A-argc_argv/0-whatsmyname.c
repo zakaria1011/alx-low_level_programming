8e41e97ebc535cdf07b9a939823c257b7d53068c
zakaria1011 <grarizakaria@gmail.com>
2023-07-08 00:30:04 +0100

    alx

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,9 +5,9 @@
  * @argc: number of lines
  * @argv: array of pointer
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-(void)argc;
+
 printf("%s \n", argv[0]);
 return (0);
 }
