e291284ce2b466bd5b2aafcd1afcd94c8b6843af
zakaria1011 <grarizakaria@gmail.com>
2023-07-08 00:18:43 +0100

    alx

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - main function
+ * Return: zero
+ * @argc: number of lines
+ * @argv: array of pointer
+ */
+int main(int argc, char *argv[])
+{
+printf("%s", argv[0]);
+return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+learning about argv and argc
