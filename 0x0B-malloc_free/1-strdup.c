01fc8e6eef1e8cf8fa666d67cd2e6090559910a5
zakaria1011 <grarizakaria@gmail.com>
2023-07-10 06:37:12 +0100

    alx

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * _strdup - return dup string
+ * @str: string to dup
+ * Return: pointer to dup str
+ */
+char *_strdup(char *str)
+{
+char *p = str;
+int i;
+char *array;
+int size = 0;
+
+while (*p != '\0')
+{
+size++;
+p++;
+}
+array = (char *)malloc(size *sizeof(char));
+if (array == NULL)
+{
+return (NULL);
+}
+for (i = 0 ; i < size ; i++)
+{
+array[i] = str[i];
+}
+return (array);
+}
