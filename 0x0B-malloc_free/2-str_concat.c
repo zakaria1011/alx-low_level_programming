39c3ca8da87f53249e6be1e6beaa7310744c1a3c
zakaria1011 <grarizakaria@gmail.com>
2023-07-10 15:29:46 +0100

    alx

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,21 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 #include<stdlib.h>
+#include<string.h>
 /**
  * str_concat - concat two strings
  * @s1: first string
  * @s2: second string
  * Return: pointer
  */
+
 char *str_concat(char *s1, char *s2)
 {
-char *p = s1, *q = s2;
-int j;
-int i;
-char *array;
-int size1 = 0;
-int size2 = 0;
-
 if (s1 == NULL)
 {
 s1 = "";
@@ -24,28 +19,14 @@ if (s2 == NULL)
 {
 s2 = "";
 }
-while (*p != '\0')
-{
-size1++;
-p++;
-}
-while (*q != '\0')
-{
-size2++;
-q++;
-}
-array = (char *)malloc((size1 + size2 + 1) * sizeof(char));
-if (array == NULL)
+int s1_len = strlen(s1);
+int s2_len = strlen(s2);
+char *result = (char *)malloc((s1_len + s2_len + 1) * sizeof(char));
+if (result == NULL)
 {
 return (NULL);
 }
-for (i = 0 ; i < size1 ; i++)
-{
-array[i] = s1[i];
-}
-for (i = size1, j = 0 ; i < size1 + size2 && j < size2 ; i++, j++)
-{
-array[i] = s2[j];
-}
-return (array);
+strcpy(result, s1);
+strcat(result, s2);
+return (result);
 }
