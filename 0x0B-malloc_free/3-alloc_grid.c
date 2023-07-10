3db0bfecf072efc1b3beab255da09a5f7f60ca24
zakaria1011 <grarizakaria@gmail.com>
2023-07-10 16:10:57 +0100

    alx

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * alloc_grid - fonction who allocat a grid
+ * @width: coloms
+ * @hight: lines
+ * Return: pointer
+ */
+int **alloc_grid(int width, int hight)
+{
+int i, j;
+int **matrix;
+if (width <= 0)
+{
+return (NULL);
+}
+if (hight <= 0)
+{
+return (NULL);
+}
+matrix = (int **)malloc(hight * sizeof(int *));
+if (matrix == NULL)
+{
+return (NULL);
+}
+for (i = 0 ; i < hight ; i++)
+{
+matrix[i] = (int *)malloc(width * sizeof(int));
+if (matrix[i] == NULL)
+{
+return (NULL);
+}
+}
+for (i = 0 ; i < hight ; i++)
+{
+for (j = 0 ; j < width ; j++)
+{
+matrix[i][j] = 0;
+}
+}
+return (matrix);
+}
