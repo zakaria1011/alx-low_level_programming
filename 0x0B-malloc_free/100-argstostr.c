2e2405a27683e7659c0c6a38563062d862fddef5
zakaria1011 <grarizakaria@gmail.com>
2023-07-11 18:24:24 +0100

    alx

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,16 +10,24 @@
  */
 char *argstostr(int ac, char **av)
 {
+int len = 0;
+char *result = NULL
+
+for (i = 0 ; i < ac ; i++)
+{
+    len += strlen(av[i]);
+}
+
+result=(char*)malloc((len+1) * sizeof(char));
 char *result = NULL;
-int i;
+
 if (ac == 0 || av == NULL)
 {
 return (NULL);
 }
+
 for (i = 0 ; i < ac ; i++)
 {
 strcat(result, av[i]);
 }
-result[ac] = '\0';
-return (result);
-}
+result[ac]='\n';
