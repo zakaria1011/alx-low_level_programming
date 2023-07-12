44de34aa981e8742e64214d4b069ee26dd4acbe3
zakaria1011 <grarizakaria@gmail.com>
2023-07-12 11:23:38 +0100

    alx

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -31,17 +31,3 @@ char *argstostr(int ac, char **av) {
 
     return result;
 }
-
-int main(int ac, char *av[])
-{
-    char *s;
-
-    s = argstostr(ac, av);
-    if (s == NULL)
-    {
-        return (1);
-    }
-    printf("%s", s);
-    free(s);
-    return (0);
-}
