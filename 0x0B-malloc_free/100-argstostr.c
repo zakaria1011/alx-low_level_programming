5a0837e084348664ba7467e3f12da1bccf2b7e99
zakaria1011 <grarizakaria@gmail.com>
2023-07-12 11:20:50 +0100

    alx

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,35 +1,48 @@
-#include "main.h"
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-/**
- * argstostr - concat all arg
- * @ac: num of line
- * @av: lines of strings
- * Return: string concat
- */
-char *argstostr(int ac, char **av)
-{
-int i; 
-int len = 0;
-char *result = NULL;
+//#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-for (i = 0 ; i < ac ; i++)
-{
-    len += strlen(av[i]);
+char *argstostr(int ac, char **av) {
+    int i, len = 0, lenindx = 0;
+    char *result = NULL;
+
+    if (ac == 0 || av == NULL) {
+        return NULL;
+    }
+
+    // Calculate the total length of the concatenated string
+    for (i = 0; i < ac; i++) {
+        len += strlen(av[i]);
+    }
+
+    result = (char *)malloc((len + ac + 1) * sizeof(char));
+    if (result == NULL) {
+        return NULL;
+    }
+
+    // Copy each argument and add a newline character
+    for (i = 0; i < ac; i++) {
+        strcat(result, av[i]);
+        lenindx += strlen(av[i]);
+        result[lenindx++] = '\n';
+    }
+
+    result[lenindx] = '\0'; // Add null-terminating character
+
+    return result;
 }
 
-result=(char*)malloc((len+1) * sizeof(char));
-
-if (ac == 0 || av == NULL)
+int main(int ac, char *av[])
 {
-return (NULL);
-}
+    char *s;
 
-for (i = 0 ; i < ac ; i++)
-{
-strcat(result, av[i]);
-}
-result[len + 1] = '\n';
-return (result);
+    s = argstostr(ac, av);
+    if (s == NULL)
+    {
+        return (1);
+    }
+    printf("%s", s);
+    free(s);
+    return (0);
 }
