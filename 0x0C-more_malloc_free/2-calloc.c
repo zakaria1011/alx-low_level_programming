0e01bf38ee8e6976ff1473d9be982021913619c7
zakaria1011 <grarizakaria@gmail.com>
2023-07-13 16:08:30 +0100

    alx

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * _calloc -  allocation set to zero
+ * @nmemb: number of celul
+ * @size: size of celul
+ * Return: pointer to an array
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+unsigned int i;
+void *result;
+if (nmemb == 0 || size == 0)
+{
+return (NULL);
+}
+result = (void *)malloc(nmemb * sizeof(size));
+if (result == NULL)
+{
+return (NULL);
+}
+for (i = 0 ; i < nmemb ; i++)
+{
+result[i] = O;
+}
+return (result);
+}
