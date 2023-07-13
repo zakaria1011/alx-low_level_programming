5269b8c197300424625318c1bfa56aaac091a7ae
zakaria1011 <grarizakaria@gmail.com>
2023-07-13 11:24:00 +0100

    alx

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,14 +13,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 unsigned int i, j, len1, len2;
 char *result;
-char empty = '\0';
 if (s1 == NULL)
 {
-s1 = &empty;
+s1 = "";
 }
 if (s2 == NULL)
 {
-s2 = &empty;
+s2 = "";
 }
 len1 = strlen(s1);
 len2 = strlen(s2);
