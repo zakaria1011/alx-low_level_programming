50c978844e29d4e0aef4bcdfa9d1c10c07598286
zakaria1011 <grarizakaria@gmail.com>
2023-07-13 14:46:57 +0100

    alx

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,6 +13,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 unsigned int i, j, len1, len2;
 char *result;
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
 len1 = strlen(s1);
 len2 = strlen(s2);
 if (n > len2)
