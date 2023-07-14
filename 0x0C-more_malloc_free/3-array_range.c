edce600dd76db50a36cfe8ca8b97d9b771effd38
zakaria1011 <grarizakaria@gmail.com>
2023-07-14 01:28:39 +0100

    alx

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+/**
+ * array_range - allocat array
+ * @min: min value
+ * @max: max value
+ * Return: pointer to int
+ */
+int *array_range(int min, int max)
+{
+int i;
+int totalsize = max - min;
+int *result;
+
+if (min > max)
+{
+return (NULL);
+}
+result = malloc(totalsize * sizeof(int));
+if (result == NULL)
+{
+return (NULL);
+}
+for (i = 0 ; i < totalsize ; i++)
+{
+result[i] = min;
+min++;
+}
+return (result);
+}
