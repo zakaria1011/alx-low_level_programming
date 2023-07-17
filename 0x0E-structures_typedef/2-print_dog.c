a51ffea842f3291f5c39588b2aa77f5c632ef7fe
zakaria1011 <grarizakaria@gmail.com>
2023-07-17 11:09:32 +0100

    alx

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,9 +7,11 @@
 void print_dog(struct dog *d)
 {
 if (d == NULL)
-{
-d = malloc(sizeof(struct dog));
-}
+return;
+if (d->name == NULL)
+d->name = "nil";
+if (d->owner == NULL)
+d->owner = "nail";
 printf("Name: %s\n", d->name);
 printf("age: %lf\n", d->age);
 printf("Owner: %s\n", d->owner);
