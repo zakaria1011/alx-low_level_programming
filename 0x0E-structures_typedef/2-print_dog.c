74c42ccaf7d5efbbdfa69b12c8f8bb85d59bfcef
zakaria1011 <grarizakaria@gmail.com>
2023-07-17 11:04:13 +0100

    alx

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,7 +6,17 @@
  */
 void print_dog(struct dog *d)
 {
-printf("Name: %s", dog->name);
-printf("age: %s", dog->age);
-printf("Owner: %s", dog->owner);
+printf("Name: %s\n", d->name);
+printf("age: %lf\n", d->age);
+printf("Owner: %s\n", d->owner);
+}
+int main(void)
+{
+    struct dog my_dog;
+
+    my_dog.name = "Poppy";
+    my_dog.age = 3.5;
+    my_dog.owner = "Bob";
+    print_dog(&my_dog);
+    return (0);
 }
