59f581366e684ec50f1418397d975d8eea1183e9
zakaria1011 <grarizakaria@gmail.com>
2023-07-17 11:01:54 +0100

    alx

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+#include<stdio.h>
+/**
+ * print_dog - print name, owner, age of a dog
+ * @d: dogg to print info of
+ */
+void print_dog(struct dog *d)
+{
+printf("Name: %s", dog->name);
+printf("age: %s", dog->age);
+printf("Owner: %s", dog->owner);
+}
