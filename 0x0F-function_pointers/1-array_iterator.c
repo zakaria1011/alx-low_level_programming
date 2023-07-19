105f6e3a5a290313f99796fb6edbc48de278179b
zakaria1011 <grarizakaria@gmail.com>
2023-07-19 11:44:01 +0100

    alx

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - function that display an array
+ * @array: array to display
+ * @size: size of an array
+ * @action: function pointer
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+int i;
+if (array && action)
+
+for (i = 0 ; i < size ; i++)
+{
+action(array[i]);
+}
+}
