46b4f12f07b78944c25fb620726509e25df5ccbf
zakaria1011 <grarizakaria@gmail.com>
2023-07-19 13:27:53 +0100

    akx

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,25 @@
+#include "3-calc.h"
+/**
+ * get_op_func - operation to do
+ * @s: symbole of the function
+ * Return : return an integer
+ */
+int (*get_op_func(char *s))(int, int)
+{
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i = 0;
+while (ops[i].op != NULL)
+{
+if(strcmp(s, ops[i].op == 0))
+return (ops[i].f);
+i++;
+}
+return NULL;
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - addition
+ * @a: first int
+ * @b: seconde int
+ * Return: result
+ */
+ /**
+ * op_sub - addition
+ * @a: first int
+ * @b: seconde int
+ * Return: result
+ */
+/**
+ * op_mul - addition
+ * @a: first int
+ * @b: seconde int
+ * Return: result
+ */
+/**
+ * op_div - addition
+ * @a: first int
+ * @b: seconde int
+ * Return: result
+ */
+/**
+ * op_mod - addition
+ * @a: first int
+ * @b: seconde int
+ * Return: result
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+int op_div(int a, int b)
+{
+return (a / b);
+}
+int op_mod(int a, int b)
+{
+return (a % b);
+}
