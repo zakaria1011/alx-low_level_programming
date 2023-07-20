816570ca3aafe75dbbf14746ef6165983c9f5365
zakaria1011 <grarizakaria@gmail.com>
2023-07-20 10:51:51 +0100

    alx

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,12 +2,12 @@
 /**
  * sum_them_all - some of n arguments
  * @n: number of arguments
+ * @...: variable arguments
  * Return: return total
  */
 int sum_them_all(const unsigned int n, ...)
 {
 unsigned int i;
-int value;
 int total = 0;
 if (n == 0)
 return (0);
@@ -15,8 +15,7 @@ va_list args;
 va_start(args, n);
 for (i = 0 ; i < n ; i++)
 {
-value = va_arg(args, int);
-total = total + value;
+total = total + va_arg(args, int);
 }
 va_end(args);
 return (total);
