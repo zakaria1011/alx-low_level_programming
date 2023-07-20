ff68e8baa028b83e85d4ce5cca914d9566e12633
zakaria1011 <grarizakaria@gmail.com>
2023-07-20 01:56:41 +0100

    alx

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -2,49 +2,49 @@
 /**
  * op_add - addition
  * @a: first num
- * @a:seconde num
- * Return : result
- */
-/**
- * op_sub - subtraction
- * @a: first num
- * @a:seconde num
- * Return : result
- */
-/**
- * op_mul - multiplication
- * @a: first num
- * @a:seconde num
- * Return : result
- */
-/**
- * op_div - divition
- * @a: first num
- * @a:seconde num
- * Return : result
- */
-/**
- * op_mod - modulo
- * @a: first num
- * @a:seconde num
- * Return : result
+ * @b:seconde num
+ * Return: result
  */
 int op_add(int a, int b)
 {
 return (a + b);
 }
+/**
+ * op_sub - subtraction
+ * @a: first num
+ * @b:seconde num
+ * Return: result
+ */
 int op_sub(int a, int b)
 {
 return (a - b);
 }
+/**
+ * op_mul - multiplication
+ * @a: first num
+ * @b:seconde num
+ * Return: result
+ */
 int op_mul(int a, int b)
 {
 return (a * b);
 }
+/**
+ * op_div - divition
+ * @a: first num
+ * @b:seconde num
+ * Return: result
+ */
 int op_div(int a, int b)
 {
 return (a / b);
 }
+/**
+ * op_mod - modulo
+ * @a: first num
+ * @b:seconde num
+ * Return: result
+ */
 int op_mod(int a, int b)
 {
 return (a % b);
