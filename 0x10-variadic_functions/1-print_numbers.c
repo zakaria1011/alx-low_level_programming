f596d2577f94b9169ae9f3e6f3cf6485e2485e7e
zakaria1011 <grarizakaria@gmail.com>
2023-07-20 11:08:40 +0100

    alx

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers -  print list of numbers
+ * @separator: string separ
+ * @n: number of arg
+ * @...: list
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list args;
+unsigned int i;
+va_start(args, n);
+int num;
+if (separator != NULL)
+{
+for (i = 0 ; i < n ; i++)
+{
+num = va_arg(args, int);
+printf("%d %s", num, separator);
+}
+}
+else
+{
+for (i = 0 ; i < n ; i++)
+{
+num = va_arg(args, int);
+printf("%d", num);
+}
+}
