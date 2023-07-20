da67af84ae0341a510eab8317fc6cb03d7a20350
zakaria1011 <grarizakaria@gmail.com>
2023-07-20 11:30:55 +0100

    alx

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,8 +9,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 va_list args;
 unsigned int i;
+
 va_start(args, n);
-int num;
 for (i = 0 ; i < n ; i++)
 {
 printf("%d", va_arg(args, int));
