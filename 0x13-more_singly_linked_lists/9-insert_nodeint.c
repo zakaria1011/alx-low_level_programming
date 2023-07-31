d59f349f6513cc4d44bdad64ac5777493273952c
zakaria1011 <grarizakaria@gmail.com>
2023-07-31 19:35:19 +0100

    alx

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,11 +11,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 listint_t *current, *new_node, *step_less;
 unsigned int i;
 
-if (head == NULL || *head == NULL)
+if (*head == NULL)
 return (NULL);
 new_node = (listint_t *)malloc(sizeof(listint_t));
 if (new_node == NULL)
 {
+free(new_node);
 return (NULL);
 }
 new_node->n = n;
