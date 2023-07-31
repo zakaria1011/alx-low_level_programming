f1d86e2e9bad3b1727ee651d049c0d27b4c9d82f
zakaria1011 <grarizakaria@gmail.com>
2023-07-31 14:15:06 +0100

    alx

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint2 - free list
+ * @head: pointer to head list
+*/
+void free_listint2(listint_t **head)
+{
+listint_t *current, *next_node;
+current = *head;
+while (current != NULL)
+{
+next_node = current->next;
+free(current);
+current = next_node;
+}
+*head = NULL;
+}
--- 0x13-more_singly_linked_lists/test.c
+++ 0x13-more_singly_linked_lists/test.c
@@ -2,6 +2,7 @@
 int main(void)
 {
     listint_t *head;
+    int *p = NULL;
 
     head = NULL;
     add_nodeint_end(&head, 0);
@@ -13,7 +14,9 @@ int main(void)
     add_nodeint_end(&head, 402);
     add_nodeint_end(&head, 1024);
     print_listint(head);
-    free_listint(head);
-    head = NULL;
+    free_listint2(&head);
+    printf("%p\n", (void *)head);
+    printf("%p", (void *)p);
+
     return (0);
 }
