71189845e5731e9b607f2ee8fd12d43d66dad5e0
zakaria1011 <grarizakaria@gmail.com>
2023-08-03 18:54:43 +0100

    alx

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,16 @@
+# include "main.h"
+/**
+ * set_bit - make bit in index 1 if not
+ * @n: arg pointer
+ * @index: an index
+ * Return: 1 or -1
+*/
+int set_bit(unsigned long int *n, unsigned int index)
+{
+if (index >= (sizeof(unsigned long int) * 8))
+{
+return (-1);
+}
+*n |= (1UL << index);
+return (1);
+}
