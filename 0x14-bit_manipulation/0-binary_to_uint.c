9e7a0b9084572893bb53b1594890481f582d18b4
zakaria1011 <grarizakaria@gmail.com>
2023-08-03 13:35:30 +0100

    alx

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,12 +10,13 @@ unsigned int sum = 0, i = 0, numValue;
 
 if (b ==  NULL)
 return (0);
-while (b != NULL)
+while (b[i] != '\0')
 {
 if (b[0] != '1' && b[i] != '0')
 return (0);
 numValue = b[i] - 'O';
 sum += numValue *pow(2, i);
+i++;
 }
 return (sum);
 }
