023298a73062845675941e81f029c47e2d617b91
zakaria1011 <grarizakaria@gmail.com>
2023-08-03 18:13:34 +0100

    alx

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_binary - find binary of number
+ * @n: arg
+*/
+void print_binary(unsigned long int n)
+{
+if (n > 1)
+print_binary(n >> 1);
+printf("%lu", (n & 1));
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 
 
 #endif
