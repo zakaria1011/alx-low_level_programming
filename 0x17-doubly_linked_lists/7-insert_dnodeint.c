4e74ee828465b3f83560d3406669f6bf28fc3632
zakaria1011 <grarizakaria@gmail.com>
2023-10-13 00:22:24 +0100

    alx

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,25 +1,32 @@
 #include "lists.h"
+/**
+ * insert_dnodeint_at_index - inset node at index
+ * @h: head of the list
+ * @idx: index
+ * @n: data
+ * Return: pointer to the new node
+*/
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *current = *h;
-    dlistint_t *prev;
-    dlistint_t *new_node;
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-    {
-        return (NULL);
-    }
-    new_node->n = n;
-    while (current != NULL && idx > 0)
-    {
-        prev = current;
-        current = current->next;
-    }
-    if (idx > 0)
-    {
-        return (NULL);
-    }
-    new_node->next = current;
-    new_node->prev = prev;
-    return (new_node);
+dlistint_t *current = *h;
+dlistint_t *prev;
+dlistint_t *new_node;
+new_node = malloc(sizeof(dlistint_t));
+if (new_node == NULL)
+{
+return (NULL);
+}
+new_node->n = n;
+while (current != NULL && idx > 0)
+{
+prev = current;
+current = current->next;
+}
+if (idx > 0)
+{
+return (NULL);
+}
+new_node->next = current;
+new_node->prev = prev;
+return (new_node);
 }
