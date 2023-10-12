29fc4e754ef1cfcc360ab2119efe03e670d9fdad
zakaria1011 <grarizakaria@gmail.com>
2023-10-12 22:58:30 +0100

    alx

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_dlistint - free nodes
+ * @head: head of the node
+*/
+void free_dlistint(dlistint_t *head)
+{
+dlistint_t *current, *prev;
+current = head;
+prev = current;
+while (current->next != NULL)
+{
+current = current->next;
+free(prev);
+prev = current;
+}
+free(current);
+}
