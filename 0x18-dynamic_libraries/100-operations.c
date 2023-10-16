f90f9b7291822c0fc387dea7b73a77db28b26c51
zakaria1011 <grarizakaria@gmail.com>
2023-10-16 19:01:26 +0100

    alx

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+/**
+ * add - adding
+ * @a: first num
+ * @b: second num
+ * Return: sum
+*/
+int add(int a, int b)
+{
+return (a + b);
+}
+/**
+ * sub - subtracting
+ * @a: first num
+ * @b: second num
+ * Return: diff
+*/
+int sub(int a, int b)
+{
+return (a - b);
+}
+/**
+ * mul - multiplication
+ * @a: first num
+ * @b: second num
+ * Return: product
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
+/**
+ * div - division
+ * @a: first num
+ * @b: second num
+ * Return: quotient
+*/
+int div(int a, int b)
+{
+if (b == 0)
+{
+return (0);
+}
+return (a / b);
+}
+/**
+ * mod - modul
+ * @a: first num
+ * @b: second num
+ * Return: rest
+*/
+int mod(int a, int b)
+{
+if (b == 0)
+{
+return (0);
+}
+return (a % b);
+}
