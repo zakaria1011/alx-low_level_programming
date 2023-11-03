bb322bcdbd8ab5682b223f985781f95b9d1406b4
zakaria1011 <grarizakaria@gmail.com>
2023-11-03 18:10:11 +0100

    alx

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,12 +17,12 @@ if (new_tab == NULL)
 {
 return (NULL);
 }
+new_tab->size = size;
 new_tab->array = malloc(sizeof(hash_node_t) * size);
 if (new_tab->array == NULL)
 {
 return (NULL);
 }
-new_tab->size = size;
 for (i = 0; i < size; i++)
 new_tab->array[i] = NULL;
 return (new_tab);
