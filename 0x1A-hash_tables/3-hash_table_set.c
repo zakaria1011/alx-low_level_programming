76ec9d72860ddf893e822e188737ca6a1d72a5cf
zakaria1011 <grarizakaria@gmail.com>
2023-11-05 14:57:49 +0100

    alx

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,26 +13,26 @@ hash_node_t *new_node, *current;
 
 if (!ht || !key || *key == '\0')
 return (0);
-index = key_index((const unsigned char *)key, ht -> size);
-current = ht -> array[index];
+index = key_index((const unsigned char *)key, ht->size);
+current = ht->array[index];
 while (current != NULL)
 {
-    if (strcmp(current -> key, key) == 0)
-    {
-        free(current -> value);
-        current -> value = strdup(value);
-        if (current -> value == NULL)
-        return (0);
-        return (1);
-    }
-    current = current -> next;
+if (strcmp(current->key, key) == 0)
+{
+free(current->value);
+current->value = strdup(value);
+if (current->value == NULL)
+return (0);
+return (1);
+}
+current = current->next;
 }
 new_node = malloc(sizeof(hash_node_t));
 if (new_node == NULL)
 return (0);
-new_node -> key = strdup(key);
-new_node -> value = strdup(value);
-new_node -> next = NULL;
-ht -> array[index] = new_node;
+new_node->key = strdup(key);
+new_node->value = strdup(value);
+new_node->next = NULL;
+ht->array[index] = new_node;
 return (1);
 }
