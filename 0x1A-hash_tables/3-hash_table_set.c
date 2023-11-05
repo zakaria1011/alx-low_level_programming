79d523c0efcbd2d1ec2b2b6ee3a2a971f1dd16a8
zakaria1011 <grarizakaria@gmail.com>
2023-11-05 01:15:06 +0100

    alx

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,10 +9,24 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 unsigned long int index;
-hash_node_t *new_node;
+hash_node_t *new_node, *current;
+char *new_value;
+
 if (!ht || !key || *key == '\0')
 return (0);
 index = key_index((const unsigned char *)key, ht -> size);
+current = ht -> array[index];
+if (current != NULL)
+{
+    if (strcmp(current -> key , key) == 0)
+    {
+        new_value = strdup(value);
+        free(current -> value);
+        current -> value == new_value;
+        return (1);
+    }
+}
+
 new_node = malloc(sizeof(hash_node_t));
 if (new_node == NULL)
 return (0);
