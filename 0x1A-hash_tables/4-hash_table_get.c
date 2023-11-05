a45be878bb3da27c1bce8401f0f5f7422f62eea9
zakaria1011 <grarizakaria@gmail.com>
2023-11-05 16:02:07 +0100

    alx

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - get a value from hash tab
+ * @ht: tab
+ * @key: the key
+ * Return: the value or null
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+unsigned long int index;
+hash_node_t *current;
+index = key_index((const unsigned char *)key, ht->size);
+current = ht->array[index];
+while (current != NULL)
+{
+if (strcmp(current->key, key) == 0)
+{
+return (current->value);
+}
+current = current->next;
+}
+return (NULL);
+}
