9e52c96df99989e1f4f923ba19d6d8466384d33d
zakaria1011 <grarizakaria@gmail.com>
2023-11-05 16:06:01 +0100

    alx

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,8 +9,6 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 unsigned long int index;
 hash_node_t *current;
-if (strcmp(key, NULL) == 0)
-return (NULL);
 index = key_index((const unsigned char *)key, ht->size);
 current = ht->array[index];
 while (current != NULL)
