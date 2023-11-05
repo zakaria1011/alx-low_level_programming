be140aa2e178bc2810fa3d7e9b1d7a249be9a633
zakaria1011 <grarizakaria@gmail.com>
2023-11-06 00:03:47 +0100

    alx

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -5,10 +5,8 @@
 */
 void hash_table_delete(hash_table_t *ht)
 {
-hash_table_t *head;
 unsigned long int index;
 hash_node_t *current, *temp;
-head = ht;
 if (ht == NULL)
 return;
 for (index = 0 ; index < ht->size ; index++)
@@ -22,7 +20,7 @@ free(current->value);
 free(current);
 current = temp;
 }
-free(head->array);
-free(head);
 }
+free(ht->array);
+free(ht);
 }
