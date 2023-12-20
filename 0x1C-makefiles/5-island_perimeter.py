54e964bbba50f0015becde7499f5acacffdf98d9
zakaria1011 <grarizakaria@gmail.com>
2023-12-20 21:46:17 +0100

    alx

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,13 +11,8 @@ def island_perimeter(grid):
         for j in range(len(grid[0])):
             if grid[i][j] == 1:
                 perimeter += 4
-
-                # Check left neighbor
                 if j > 0 and grid[i][j - 1] == 1:
                     perimeter -= 2
-
-                # Check top neighbor
                 if i > 0 and grid[i - 1][j] == 1:
                     perimeter -= 2
-
     return perimeter
